4a52857aeb93685a777c2243b01626b5380e21d2
Jesus Peralta <jesus_web_master@hotmail.com>
2025-06-25 11:02:52 -0600

    html base finished

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,12 +4,10 @@ import { NavLink, useLocation } from "react-router-dom"
 export default function Header() {
 
     const { pathname } = useLocation()
-    console.log( pathname )
-
     const isHome = useMemo( () => pathname === '/' , [ pathname ] )
 
   return (
-    <header className="bg-slate-800">
+    <header className={ isHome ? 'bg-header bg-center bg-cover' : 'bg-slate-800'}>
         <div className="mx-auto container px-5 py-16">
             <div className="flex justify-between items-center">
                 <div>
