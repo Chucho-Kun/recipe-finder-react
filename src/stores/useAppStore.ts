db2e35c334cf9a2f7ee62b826c734c9c34a3b013
Jesus Peralta <jesus_web_master@hotmail.com>
2025-06-25 11:19:15 -0600

    multiple state by slice pattern

---
+++ src/stores/useAppStore.ts
@@ -0,0 +1,6 @@
+import { create } from "zustand";
+import { createRecipiesSlice, type RecipiesSliceType } from './recipeSlice';
+
+export const useAppStore = create<RecipiesSliceType>( (...a) => ({
+    ...createRecipiesSlice(...a)
+}) )
--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -1,7 +1,11 @@
-import Header from "../components/Header";
+import { useAppStore } from "../stores/useAppStore"
 
+const { categories } = useAppStore()
 
 export default function indexPage() {
+
+  categories
+
   return (
     <>
        <h1>Inicio</h1>
