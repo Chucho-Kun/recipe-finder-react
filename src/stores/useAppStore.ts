91262ce0b4fa432bbea387980d17fac60da30d6e
Jesus Peralta <jesus_web_master@hotmail.com>
2025-06-27 09:14:06 -0600

    favorites slide pattern

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,7 +1,10 @@
 import { create } from "zustand";
 import { devtools } from 'zustand/middleware'
 import { createRecipiesSlice, type RecipiesSliceType } from './recipeSlice';
+import { createFavoritesSlice, FavoritesSliceType } from './favoriteSlice';
 
-export const useAppStore = create<RecipiesSliceType>()(devtools( (...a) => ({
-    ...createRecipiesSlice(...a)
+
+export const useAppStore = create<RecipiesSliceType & FavoritesSliceType>()(devtools( (...a) => ({
+    ...createRecipiesSlice(...a),
+    ...createFavoritesSlice(...a)
 }) ))
